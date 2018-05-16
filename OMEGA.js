bde886a512771592789de24528bf6fdfe7b3cd2d
TheGitHub428Guy <hiim11354428@gmail.com>
2018-05-16 18:15:30 -0400

    Make the JS file
    yay

---
+++ OMEGA.js
@@ -0,0 +1,30 @@
+/* Notes: 
+1. This code is probably messy but I don't mind. It works, right?
+*/
+// Initialization of variables
+var units;
+var units = 0;
+var tickms;
+var tickms = 33;
+
+// Functions
+function updateUnits() { // Update the units text.
+	if (units.toString(10).indexOf(".") == -1) {
+		document.getElementById("units").innerHTML = units.toString(10);
+	} else {
+		document.getElementById("units").innerHTML = units.toString(10).slice(0, units.toString(10).indexOf(".") + 4);
+		while (document.getElementById("units").innerHTML[-1] == "0" || document.getElementById("units").innerHTML[-1] == ".") {
+			document.getElementById("units").innerHTML.pop();
+		}
+	}
+}
+
+function buy(g) { // Buy a certain generator.
+	alert("This is a work in (not much) progress right now.");
+}
+
+function tick(s) { // Called whenever 1 tick passes by.
+	units += s;
+	updateUnits()
+}
+setInterval(tick, tickms, (tickms / 1000));
