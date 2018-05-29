bdcdd581da96a8af79e30c08570af9343ca9910b
Four Two Eight <hiim11354428@gmail.com>
2018-05-29 13:08:57 -0400

    BreakOmega implementation!
    Yay also save net neutrality pleas

--- OMEGA.js
+++ OMEGA.js
@@ -2,12 +2,15 @@
 1. This code is probably messy but I don't mind. It works, right?
 */
 
-//Break Omega (oh gosh)
+//Break Omega
 function BrOmega(mantissa, exponent) {  // Mantissa also has sign (like -5). Exponent can be negative.
-	//help me
-	//aaaaa
-	this.mantissa = mantissa
-	this.exponent = exponent
+	/* Initialization first, of course */
+	this.mantissa = mantissa * (10 ** (exponent % 1))
+	this.exponent = Math.floor(exponent)
+	if (this.mantissa == 0 || this.exponent == -Infinity) {
+		this.mantissa = 1
+		this.exponent = -Infinity
+	}
 	while (Math.abs(this.mantissa) >= 10) {
 		this.exponent += 1
 		this.mantissa /= 10
@@ -16,38 +19,115 @@ function BrOmega(mantissa, exponent) {  // Mantissa also has sign (like -5). Exp
 		this.exponent -= 1
 		this.mantissa *= 10
 	}
-	if (this.mantissa == 0) {
-		this.exponent = -Infinity
+	/* TIME FOR FUNCTIONS */
+	this.add = function (other) {
+		if (other.exponent > this.exponent) {
+			return new BrOmega(other.mantissa + (this.mantissa * (10 ** (this.exponent - other.exponent))), other.exponent)
+		} else {
+			return new BrOmega(this.mantissa + (other.mantissa * (10 ** (other.exponent - this.exponent))), this.exponent)
+		}
 	}
-}
-
-// Initialization of variables
-var units; var units = 1;
-var tickms; var tickms = 33;
-var gens; var gens = [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]] // First list is generator amount; Second list is amount brought.
-var genCosts; var genCosts = [[1, 10, 100, 1000, 1e+5, 1e+7, 1e+9, 1e+11], [2, 3, 4, 6, 16, 24, 32, 64]] // First list is generator cost base; second list is generator cost scaling.
-
-// Functions
-function updateUnits() { // Update the units text.
-	if (units.toString(10).indexOf(".") == -1) {
-		document.getElementById("units").innerHTML = units.toString(10);
-	} else {
-		document.getElementById("units").innerHTML = units.toString(10).slice(0, units.toString(10).indexOf(".") + 4);
-		while (document.getElementById("units").innerHTML[-1] == "0" || document.getElementById("units").innerHTML[-1] == ".") {
-			document.getElementById("units").innerHTML.pop();
+	this.sub = function (other) {
+		return this.add(new BrOmega(-other.mantissa, other.exponent))
+	}
+	this.mul = function (other) {
+		return new BrOmega(this.mantissa * other.mantissa, this.exponent + other.exponent)
+	}
+	this.div = function (other) {
+		return new BrOmega(this.mantissa / other.mantissa, this.exponent - other.exponent)
+	}
+	this.pow = function (other) { //use an integer for "other" in this one
+		let logten = 0;
+		if (this.mantissa == 0) {
+			return new BrOmega(0, 0)
+		} else {
+			if (Math.log10(10) == 1) {
+				logten = Math.log10(Math.abs(this.mantissa));
+			} else {
+				logten = Math.log(Math.abs(this.mantissa)) / Math.log(10);
+			}
+			let almostdone = new BrOmega(10 ** ((logten * other) % 1), (this.exponent * other) + Math.floor(logten * other));
+			return new BrOmega(almostdone.mantissa * ((this.mantissa / Math.abs(this.mantissa)) ** other), almostdone.exponent);
+		}
+	}
+	this.abs = function () {
+		return new BrOmega(Math.abs(this.mantissa), this.exponent)
+	}
+	this.eq = function (other) {
+		return (this.mantissa == other.mantissa) && (this.exponent == other.exponent)
+	}
+	this.gt = function (other) {
+		return (((this.sub(other)).mantissa > 0) && !((this.sub(other)).exponent == -Infinity))
+	}
+	this.lt = function (other) {
+		return other.gt(this)
+	}
+	this.toSci = function (plus, digitsRight, digitsLimit) { // This one was the hardest one so far
+		/* The "plus" parameter determines if it's like 5e9 or 5e+9. 
+		The "digitsRight" parameter determines how many digits are to the right of the decimal point. Example: 6.856e+8 -> 6.85e+8.
+		The "digitsLeft" parameter determines how big/small the number has to be until it switches to scientific notation. Example: 12345 -> 1.23e4.*/
+		let sciResult = "";
+		if (Math.abs(this.exponent) > digitsLimit) {
+			if (plus && (this.exponent >= 0)) { 
+				if ((this.mantissa.toString(10).indexOf(".") == -1) || (this.mantissa.toString(10).length < this.mantissa.toString(10).indexOf(".") + digitsRight + 3)) {
+					sciResult = (this.mantissa.toString(10) + "e+" + this.exponent.toString(10)) 
+				} else {
+					sciResult = (this.mantissa.toString(10).slice(0, this.mantissa.toString(10).indexOf(".") + digitsRight + 1) + "e+" + this.exponent.toString(10)) 
+				}
+			} else { 
+				if ((this.mantissa.toString(10).indexOf(".") == -1) || (this.mantissa.toString(10).length < this.mantissa.toString(10).indexOf(".") + digitsRight + 3)) {
+					sciResult = (this.mantissa.toString(10) + "e" + this.exponent.toString(10)) 
+				} else {
+					sciResult = (this.mantissa.toString(10).slice(0, this.mantissa.toString(10).indexOf(".") + digitsRight + 1) + "e" + this.exponent.toString(10)) 
+				}
+			}
+		} else {
+			if (((this.mantissa * (10 ** this.exponent)).toString(10).indexOf(".") == -1) || ((this.mantissa * (10 ** this.exponent)).toString(10).length < this.mantissa.toString(10).indexOf(".") + digitsRight + 3)) {
+				sciResult = ((this.mantissa * (10 ** this.exponent)).toString(10))
+			} else {
+				sciResult = ((this.mantissa * (10 ** this.exponent)).toString(10).slice(0, this.mantissa.toString(10).indexOf(".") + digitsRight + 1))
+			}
+		}
+		if (sciResult[sciResult.length - 1] == ".") {
+			sciResult = sciResult.slice(0, sciResult.length - 1);
+		}
+		if (this.exponent == -Infinity) {
+			return "0";
+		} else {
+			return sciResult;
 		}
 	}
 }
 
+// Initialization of variables
+var units; var units = new BrOmega(1, 0);
+var tickms; var tickms = 33;
+var gens; var gens = [[new BrOmega(0, 0), new BrOmega(0, 0), new BrOmega(0, 0), new BrOmega(0, 0), new BrOmega(0, 0), new BrOmega(0, 0), new BrOmega(0, 0), new BrOmega(0, 0)], [0, 0, 0, 0, 0, 0, 0, 0]] // First list is generator amount; Second list is amount brought.
+var genCosts; var genCosts = [[new BrOmega(1, 0), new BrOmega(1, 1), new BrOmega(1, 2), new BrOmega(1, 3), new BrOmega(1, 5), new BrOmega(1, 7), new BrOmega(1, 9), new BrOmega(1, 11)], [new BrOmega(2, 0), new BrOmega(3, 0), new BrOmega(4, 0), new BrOmega(6, 0), new BrOmega(16, 0), new BrOmega(24, 0), new BrOmega(32, 0), new BrOmega(64, 0)]] // First list is generator cost base; second list is generator cost scaling.
+
+// Functions
+
 function buy(gen) { // Buy a certain generator.
-	console.log("WIP");
+	if (!(units.lt(genCosts[0][gen].mul(genCosts[1][gen].pow(gens[1][gen]))))) {
+		units = units.sub(genCosts[0][gen].mul(genCosts[1][gen].pow(gens[1][gen])))
+		console.log(genCosts[0][gen].mul(genCosts[1][gen].pow(gens[1][gen])).toSci(false, 5, 10))
+		gens[1][gen] += 1;
+		gens[0][gen] = gens[0][gen].add(new BrOmega(1, 0))
+		document.getElementById("buyAlpha").innerHTML = "Cost: " + genCosts[0][gen].mul(genCosts[1][gen].pow(gens[1][gen])).toSci(false, 2, 4)
+	}
+}
+
+function calcMul(gen) {
+	return new BrOmega(1, 0);
 }
 
 function tick(seconds) { // Called whenever 1 tick passes by.
-	units += seconds * Math.floor(gens[0][0]);
+	units = units.add((new BrOmega(seconds, 0)).mul(gens[0][0]));
 	for (let i=0; i < 7; i++) {
-		gens[0][i] += seconds * Math.floor(gens[0][i+1]);
+		gens[0][i] = gens[0][i].add((new BrOmega(seconds, 0)).mul(gens[0][i+1]))
+		// gens[0][i] += seconds * Math.floor(gens[0][i+1]);
 	}
-	updateUnits()
+	document.getElementById("units").innerHTML = units.toSci(false, 2, 3);
+	document.getElementById("alpha").innerHTML = gens[0][0].toSci(false, 2, 3) + "(" + gens[1][0].toString(10) + ")";
 }
 setInterval(tick, tickms, (tickms / 1000));
